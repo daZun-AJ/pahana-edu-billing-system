19585479c75c8539432658090851ec61c4906d4e
Dasun Anjana <dasuna39@gmail.com>
2025-08-18 02:06:27 +0530

    Create DB Connection

---
+++ src/main/java/com/pahanaedu/dao/DBConnectionFactory.java
@@ -0,0 +1,11 @@
+package com.pahanaedu.dao;
+
+import java.sql.Connection;
+
+public class DBConnectionFactory {
+
+	public static Connection getConnection() {
+		return DBConnection.getInstance().getConnection();
+	}
+	
+}
